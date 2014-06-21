ad1aa259ab8bb7f392d981ca939509452d3ed2cd
Mirko Mignini <mirko.mignini@libero.it>
2014-06-21 09:51:16 +0200

    changed where function for issue with postgre

--- lib/acts_as_default.rb
+++ lib/acts_as_default.rb
@@ -69,7 +69,7 @@ module ActsAsDefault
         self.class.where(default: true).first
       else
         key = self.class.field_key
-        self.class.where("#{key} = :key and default = :default", {key: self[key], default: true}).first
+        self.class.where("#{key} = :key and default = :default", {key: self[key], default: 'true'}).first
       end
     end
   end
